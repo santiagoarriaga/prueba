f63241d7836723c317c8631aec1854dea607384f
Santiago <santiago.noguez@risevision.com>
2021-10-08 17:12:14 -0500

    ejemplo interpolación

---
+++ interpolacion01.js
@@ -0,0 +1,9 @@
+const nombre = "Santiago"
+const apellido = "Arriaga"
+const pais = "Mexico"
+
+const cadena = nombre + "-" + apellido + "-" + pais
+const cadena2 = `${ nombre } - ${ apellido } - ${ pais }`
+
+console.log( cadena )
+console.log( cadena2 )
