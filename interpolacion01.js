75c99f355b525f586dcb0f8a7b2a087a9a9627fa
Santiago <santiago.noguez@risevision.com>
2021-10-08 17:49:50 -0500

    expresion en interpolacion

--- interpolacion01.js
+++ interpolacion01.js
@@ -4,6 +4,8 @@ const pais = "Mexico"
 
 const cadena = nombre + "-" + apellido + "-" + pais
 const cadena2 = `${ nombre } - ${ apellido } - ${ pais }`
+const cadena3 = `${ 1 + 1 } - ${ Math.abs( -1 ) }`
 
 console.log( cadena )
 console.log( cadena2 )
+console.log( cadena3 )
