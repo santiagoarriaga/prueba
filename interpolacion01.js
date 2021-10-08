a06afb3c23cfc79e7911f7d1cd2f558ccbce863b
Santiago <santiago.noguez@risevision.com>
2021-10-08 17:50:24 -0500

    cambio

--- interpolacion01.js
+++ interpolacion01.js
@@ -5,6 +5,8 @@ const pais = "Mexico"
 const cadena = nombre + "-" + apellido + "-" + pais
 const cadena2 = `${ nombre } - ${ apellido } - ${ pais }`
 const cadena3 = `${ 1 + 1 } - ${ Math.abs( -1 ) }`
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 console.log( cadena )
 console.log( cadena2 )
